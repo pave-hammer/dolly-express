63c29a6f2750e1ce47c777c0055306a4a593ee46
Mike <szekleym@gmail.com>
2019-01-17 14:58:47 -0700

    finished get random logic - working as intended

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express")
 const app = express()
 const port = 3003
 const data = require("./data.json")
+const cors = require("cors")
 
-app.use(express.static("public"))
+app.use(cors())
 
 app.get("/data", (req, res, next) =>{
   res.status(200).send({
@@ -13,7 +14,17 @@ app.get("/data", (req, res, next) =>{
 })
 
 app.get("/:tag", (req, res, next) => {
-  res.status(200).send(req.params.tag)
+  const tag = req.params.tag
+  if(!data.tags.includes(tag)){
+    res.status(404).send("No data matching your search term.")
+  } else {
+    const matching = data.songs.filter(songs => songs.tags.includes(tag))
+    res.status(200).send(matching)
+  }
+})
+
+app.use((req, res, next) => {
+  res.status(404).send("Path not found.")
 })
 
 app.listen(port, () => console.log(`Porty on port ${port}!`))
